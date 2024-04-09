5d6c3a9427fe19105e8b2dafee5fab759adf9cb8
Nikola Dorde Glisic <ndglisic@lis.eu>
2024-04-09 14:02:18 +0200

    Added NG comment

--- src/DemoProject.Console/Program.cs
+++ src/DemoProject.Console/Program.cs
@@ -17,23 +17,24 @@ public class Program
 
     if (Match.Success)
     {
-      int Year = Convert.ToInt32(Match.Groups[0].Value.Split('-')[0]); 
+      int Year = Convert.ToInt32(Match.Groups[0].Value.Split('-')[0]);
       int Month = Convert.ToInt32(Match.Groups[0].Value.Split('-')[1]);
       int Day = Convert.ToInt32(Match.Groups[0].Value.Split('-')[2]);
 
       Console.WriteLine($"The {Input} was a {CalculateDayOfWeek(Year, Month, Day)}");
     }
-    else 
+    else
       Console.WriteLine("WRONG FORMAT");
   }
 
   private static DayOfWeek CalculateDayOfWeek(int year, int month, int day)
   {
+    //TestNg
     if (month < 3)
       year--;
 
     int weekDay = Convert.ToInt32((
-      (day + Math.Floor(2.6 * ((month + 9) % 12 + 1 - 1) - 0.2) + year % 100 
+      (day + Math.Floor(2.6 * ((month + 9) % 12 + 1 - 1) - 0.2) + year % 100
       + Math.Floor(((double)year % 100 / 4)) + Math.Floor((double)year / 400) - 2
       * Math.Floor((double)year / 100) - 1) % 7 + 7) % 7 + 1);
 
